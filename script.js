189878641a0d1f80fe28ab089d9ae81be6137e6a
aliciapaz <aliciapazrojas@gmail.com>
2021-04-15 16:29:12 -0400

    handle book form data

--- script.js
+++ script.js
@@ -27,8 +27,38 @@ function deleteBook(book) {
     myLibrary.splice(myLibrary.indexOf(book), 1);
 }
 
-var addBookButton = document.getElementById("addBookButton");
-var addBookModal = document.getElementById("addBookModal");
+let table = document.getElementById('table');
+
+function addRow(book) {
+    let row = table.insertRow();
+    let titleCell = row.insertCell();
+    let authorCell = row.insertCell();
+    let pagesCell = row.insertCell();
+    let readCell = row.insertCell();
+    let deleteCell = row.insertCell();
+    titleCell.innerHTML = book.title;
+    authorCell.innerHTML = book.author;
+    pagesCell.innerHTML = book.pages;
+    readCell.innerHTML = book.read ? 'Read' : 'Not read yet';
+    deleteCell.innerHTML = 'Delete';
+}
+
+let addBookButton = document.getElementById("addBookButton");
+let addBookModal = document.getElementById("addBookModal");
+let form = document.getElementById("bookForm");
+let submitBtn = document.getElementById("submit");
+
+submitBtn.addEventListener('submit', (event) => {
+    let title = form.element['title'].value;
+    let author = form.element['author'].value;
+    let pages = form.element['pages'].value;
+    let read = form.element['read'].value;
+    myBook = new Book(title, author, pages, read);
+    addBookToLibrary(myBook)
+    addRow(myBook)
+});
+
+// book variables 
 
 addBookButton.onclick = function(){
     addBookModal.style.display = "block";
@@ -40,5 +70,4 @@ window.onclick = function(event) {
     }
 }
 
-
-
+console.log(myLibrary)
