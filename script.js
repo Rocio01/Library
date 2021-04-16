f42f308ce4f34cb3c519b39989ec61a18b1563ef
aliciapaz <aliciapazrojas@gmail.com>
2021-04-16 11:35:14 -0400

    add delete button in every row

--- script.js
+++ script.js
@@ -1,73 +1,76 @@
-'use strict'; 
+"use strict";
 
 let myLibrary = [];
 if (localStorage.getItem("books")) {
-    myLibrary = JSON.parse(localStorage.getItem("books"));
-} 
-
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read
+  myLibrary = JSON.parse(localStorage.getItem("books"));
 }
 
-Book.prototype.changeRead = function(read) {
-     this.read = this.read ? false : true; 
-} 
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
 
-function addBookToLibrary(book){
- myLibrary.push(book);
- localStorage.setItem("books", JSON.stringify(myLibrary));
+Book.prototype.changeRead = function (read) {
+  this.read = this.read ? false : true;
+};
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  book.id = myLibrary.indexOf(book)
+  localStorage.setItem("books", JSON.stringify(myLibrary));
 }
 
 function deleteBook(book) {
-    myLibrary.splice(myLibrary.indexOf(book), 1);
-    
+  myLibrary.splice(myLibrary.indexOf(book), 1);
 }
 
 let addBookButton = document.getElementById("addBookButton");
 let addBookModal = document.getElementById("addBookModal");
 let form = document.getElementById("bookForm");
 
-form.addEventListener('submit', (e) => {
-
-    e.preventDefault();
-    let title = form.elements['title'].value;
-    let author = form.elements['author'].value;
-    let pages = form.elements['pages'].value;
-    let read = form.elements['readStatus'].value;
-    let myBook = new Book(title, author, pages, read);
-    addBookToLibrary(myBook);
-    addRow(myBook);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let title = form.elements["title"].value;
+  let author = form.elements["author"].value;
+  let pages = form.elements["pages"].value;
+  let read = form.elements["readStatus"].value;
+  let myBook = new Book(title, author, pages, read);
+  addBookToLibrary(myBook);
+  addRow(myBook);
 });
 
-
-
-let table = document.getElementById('table');
+let table = document.getElementById("table");
 
 function addRow(book) {
-    let row = table.insertRow();
-    let titleCell = row.insertCell();
-    let authorCell = row.insertCell();
-    let pagesCell = row.insertCell();
-    let readCell = row.insertCell();
-    let deleteCell = row.insertCell();
-    titleCell.innerHTML = book.title;
-    authorCell.innerHTML = book.author;
-    pagesCell.innerHTML = book.pages;
-    readCell.innerHTML = book.read === true ? 'Read' : 'Not read yet';
-    deleteCell.innerHTML = 'Delete';
+  let row = table.insertRow();
+  let titleCell = row.insertCell();
+  let authorCell = row.insertCell();
+  let pagesCell = row.insertCell();
+  let readCell = row.insertCell();
+  let deleteCell = row.insertCell();
+  deleteCell.id = `deleteCell${book.id}`;
+  titleCell.innerHTML = book.title;
+  authorCell.innerHTML = book.author;
+  pagesCell.innerHTML = book.pages;
+  readCell.innerHTML = book.read === true ? "Read" : "Not read yet";
+  addDeleteBtn(book);
 }
 
-addBookButton.onclick = function(){
-    addBookModal.style.display = "block";
+function addDeleteBtn(book) {
+  let deleteBtn = document.createElement("button");
+  let deleteCell = document.getElementById(`deleteCell${book.id}`);
+  deleteBtn.innerHTML = "Delete";
+  deleteCell.appendChild(deleteBtn);
 }
 
-window.onclick = function(event) {
-    if (event.target === addBookModal) {
-        addBookModal.style.display = "none";
-    }
-}
+addBookButton.onclick = function () {
+  addBookModal.style.display = "block";
+};
 
-
+window.onclick = function (event) {
+  if (event.target === addBookModal) {
+    addBookModal.style.display = "none";
+  }
+};
